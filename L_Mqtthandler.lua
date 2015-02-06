23c4016313352e47d63c38c04bcdab9e373937fc
Averelll <Ferd.scheepers@mamahuhu.nl>
2015-02-06 12:21:56 +0100

    v0.2
    Added control for heating, fixed windows controls.

--- L_Mqtthandler.lua
+++ L_Mqtthandler.lua
@@ -26,15 +26,29 @@ local function log(text, level)
 
 function mqttsend(mqttdata)
 	luup.log(MQTTHANDLER_LOG_NAME .. "Publish MQTT message: " .. mqttdata, 1)
-	mqtt_client:publish("Vera/Test", "sent : " .. mqttdata)
+	mqtt_client:publish("Event/Vera", mqttdata)
 end
 
 function mqttcallback (topic, payload)
 	luup.log(MQTTHANDLER_LOG_NAME .. "Enter readFromMqtt", 1)
 	luup.log(topic .. ":" .. payload,1)
 	local testj = json:decode(payload)
-	luup.variable_set("urn:upnp-org:serviceId:TemperatureSensor1", "CurrentTemperature", testj.temperature, tonumber(testj.id))
-	luup.log(testj.temperature)
+	if luup.devices[tonumber(testj.id)].category_num == 17 then
+		luup.variable_set("urn:upnp-org:serviceId:TemperatureSensor1", "CurrentTemperature", testj.temperature, tonumber(testj.id))
+	end
+	if luup.devices[tonumber(testj.id)].category_num == 16 then
+		luup.log("humidity")
+		luup.log(testj.humidity)
+		luup.variable_set("urn:micasaverde-com:serviceId:HumiditySensor1", "CurrentLevel", testj.humidity, tonumber(testj.id))
+	end
+	if luup.devices[tonumber(testj.id)].category_num == 5 then
+		luup.log("heater")
+--		luup.log(testj.humidity)
+		luup.variable_set("urn:upnp-org:serviceId:TemperatureSensor1", "CurrentTemperature", testj.temperature, tonumber(testj.id))
+		luup.variable_set("urn:upnp-org:serviceId:TemperatureSetpoint1_Heat", "CurrentSetpoint", testj.setpoint, tonumber(testj.id))
+	end
+	luup.log(luup.devices[tonumber(testj.id)].category_num)
+	luup.log(testj.id)
 end
 
 function startup(lul_device)
@@ -46,9 +60,12 @@ function startup(lul_device)
 	luup.chdev.append(lul_device, child_devices, "T1", "Bathroom", "urn:schemas-micasaverde-com:device:TemperatureSensor:1", "D_TemperatureSensor1.xml", "", "urn:upnp-org:serviceId:TemperatureSensor1,CurrentTemperature=0", true)
 	luup.chdev.append(lul_device, child_devices, "Phigh", "Power High", "urn:schemas-micasaverde-com:device:PowerMeter:1", "D_PowerMeter1.xml", "", "urn:micasaverde-com:serviceId:EnergyMetering1,KWH=1331.1", true)
 	luup.chdev.append(lul_device, child_devices, "Plow", "Power Low", "urn:schemas-micasaverde-com:device:PowerMeter:1", "D_PowerMeter1.xml", "", "urn:micasaverde-com:serviceId:EnergyMetering1,KWH=1331.23", true)
-	luup.chdev.append(lul_device,child_devices,"Blind 1","Rolluiken boven links","urn:schemas-micasaverde-com:device:WindowCovering:1","D_WindowCovering1.xml","","",false)
-	luup.chdev.append(lul_device,child_devices,"Blind 2","Rolluiken boven rechts","urn:schemas-micasaverde-com:device:WindowCovering:1","D_WindowCovering1.xml","","",false)
-	luup.chdev.append(lul_device, child_devices, "T2", "Shed", "urn:schemas-micasaverde-com:device:TemperatureSensor:1", "D_TemperatureSensor1.xml", "", "urn:upnp-org:serviceId:TemperatureSensor1,CurrentTemperature=0", true)
+	luup.chdev.append(lul_device, child_devices, "Blind 1","Rolluiken boven links","urn:schemas-micasaverde-com:device:WindowCovering:1","D_WindowCovering1.xml","","",false)
+	luup.chdev.append(lul_device, child_devices, "Blind 2","Rolluiken boven rechts","urn:schemas-micasaverde-com:device:WindowCovering:1","D_WindowCovering1.xml","","",false)
+	luup.chdev.append(lul_device, child_devices, "T2", "Temperature Shed", "urn:schemas-micasaverde-com:device:TemperatureSensor:1", "D_TemperatureSensor1.xml", "", "urn:upnp-org:serviceId:TemperatureSensor1,CurrentTemperature=0", true)
+	luup.chdev.append(lul_device, child_devices, "Blind 3","Rolluiken keuken","urn:schemas-micasaverde-com:device:WindowCovering:1","D_WindowCovering1.xml","","",false)
+	luup.chdev.append(lul_device, child_devices, "Hum 1", "Humidity Shed","urn:micasaverde-com:serviceId:HumiditySensor1", "D_HumiditySensor1.xml", "S_HumiditySensor1.xml", "", true)
+	luup.chdev.append(lul_device, child_devices, "Heat 1", "Thermostat kitchen","urn:schemas-upnp-org:device:Heater:1", "D_Heater1.xml", "", "", true)
 	luup.chdev.sync(lul_device,child_devices)
 
 -- We want to watch all zwave switches and dimmers --
@@ -66,6 +83,7 @@ for deviceNo,d in pairs(luup.devices) do
 		end
 	end
 end
+luup.variable_watch("watchVariable", "urn:upnp-org:serviceId:SwitchPower1", "Status", 58)
 
 	_G["sendToMqtt"] = sendToMqtt
 	_G["startMqtt"] = startMqtt
@@ -125,7 +143,7 @@ end
 
 function watchVariable(lul_device, lul_service, lul_variable, lul_value_old, lul_value_new)
 	luup.log(MQTTHANDLER_LOG_NAME .. "Device: " .. lul_device .. " Variable: " .. lul_variable .. " Value " .. lul_value_old .. " => " .. lul_value_new, 1)
-		local json_string = "{\"device_id\": " ..lul_device .. ", \"variable\": \"" .. lul_variable .. "\", \"newvalue\": " .. lul_value_new .. "}"
+		local json_string = "{\"id\": " ..lul_device .. ", \"variable\": \"" .. lul_variable .. "\", \"newvalue\": " .. lul_value_new .. "}"
 	mqtt_client:publish("Event/Vera", json_string)
 end
 
